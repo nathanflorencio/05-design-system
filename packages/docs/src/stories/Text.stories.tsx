3da26f2041cd7a9e84b6f724edb27ad82ee24bdd
Nathan Florencio <nflorencio@showcasepro.com.br>
2023-09-25 16:03:12 -0300

    Finished Storybook: Text Component.

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi adipisci quam eos officia recusandae aliquid ea molestiae, corporis qui, sit fugit fugiat pariatur reprehenderit, et nobis consequatur repudiandae consequuntur sapiente?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
