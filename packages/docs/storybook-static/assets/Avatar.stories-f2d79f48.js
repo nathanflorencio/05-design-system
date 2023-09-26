d8135086df5e2e28c8f2a1de03b77ef98ed711de
Nathan Florencio <nflorencio@showcasepro.com.br>
2023-09-26 12:52:17 -0300

    Add build with .nojekyll

---
+++ packages/docs/storybook-static/assets/Avatar.stories-f2d79f48.js
@@ -0,0 +1,6 @@
+import{A as i}from"./index-0c0b9aac.js";import"./index-c0290abd.js";import"./_commonjsHelpers-725317a4.js";import"./extends-98964cd2.js";import"./jsx-runtime-5fc188ad.js";const g={title:"Data display/Avatar",component:i,tags:["autodocs"],args:{src:"https://github.com/nathanflorencio.png",alt:"Nathan Florencio"},argTypes:{src:{control:{type:"text"}}}},r={},a={args:{src:void 0}};var t,o,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var e,c,n;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  args: {
+    src: undefined
+  }
+}`,...(n=(c=a.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};const h=["Primary","WithFallback"];export{r as Primary,a as WithFallback,h as __namedExportsOrder,g as default};
+//# sourceMappingURL=Avatar.stories-f2d79f48.js.map
