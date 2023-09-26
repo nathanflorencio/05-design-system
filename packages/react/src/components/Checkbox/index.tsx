ddf217205e470b543eed876eda2d1d756b16d7c2
Nathan Florencio <nflorencio@showcasepro.com.br>
2023-09-26 09:24:45 -0300

    Finished Storybook: Checkbox Component.

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,17 @@
+import { Check } from 'phosphor-react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { ComponentProps } from 'react'
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {}
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
