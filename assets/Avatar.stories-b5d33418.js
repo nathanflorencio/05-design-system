4f0ab487a19aa0d4af36108a3eb364d1e9c09495
Nathan Florencio <nflorencio@showcasepro.com.br>
2023-09-26 12:55:12 -0300

    Revert "Deploy Storybook to GitHub Pages"
    This reverts commit 492821373fa3755f2bfeed5729c6bbb108d40b94.

--- assets/Avatar.stories-b5d33418.js
+++
@@ -1,6 +0,0 @@
-import{A as i}from"./index-8c31961e.js";import"./index-c0290abd.js";import"./_commonjsHelpers-725317a4.js";import"./extends-98964cd2.js";import"./jsx-runtime-5fc188ad.js";const g={title:"Data display/Avatar",component:i,tags:["autodocs"],args:{src:"https://github.com/nathanflorencio.png",alt:"Nathan Florencio"},argTypes:{src:{control:{type:"text"}}}},r={},a={args:{src:void 0}};var t,o,s;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:"{}",...(s=(o=r.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};var e,c,n;a.parameters={...a.parameters,docs:{...(e=a.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  args: {
-    src: undefined
-  }
-}`,...(n=(c=a.parameters)==null?void 0:c.docs)==null?void 0:n.source}}};const h=["Primary","WithFallback"];export{r as Primary,a as WithFallback,h as __namedExportsOrder,g as default};
-//# sourceMappingURL=Avatar.stories-b5d33418.js.map
--- assets/Text.stories-6682df89.js
+++
@@ -1,7 +0,0 @@
-import{a as c}from"./index-8c31961e.js";import"./index-c0290abd.js";import"./_commonjsHelpers-725317a4.js";import"./extends-98964cd2.js";import"./jsx-runtime-5fc188ad.js";const x={title:"Typography/Text",component:c,tags:["autodocs"],args:{size:"md",children:"Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi adipisci quam eos officia recusandae aliquid ea molestiae, corporis qui, sit fugit fugiat pariatur reprehenderit, et nobis consequatur repudiandae consequuntur sapiente?"},argTypes:{size:{options:["xxs","xs","sm","md","lg","xl","2xl","3xl","4xl","5xl","6xl","7xl","8xl","9xl"],control:{type:"inline-radio"}}}},r={},e={args:{children:"Strong text",as:"strong"}};var s,t,a;r.parameters={...r.parameters,docs:{...(s=r.parameters)==null?void 0:s.docs,source:{originalSource:"{}",...(a=(t=r.parameters)==null?void 0:t.docs)==null?void 0:a.source}}};var o,i,n;e.parameters={...e.parameters,docs:{...(o=e.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  args: {
-    children: 'Strong text',
-    as: 'strong'
-  }
-}`,...(n=(i=e.parameters)==null?void 0:i.docs)==null?void 0:n.source}}};const g=["Primary","CustomTag"];export{e as CustomTag,r as Primary,g as __namedExportsOrder,x as default};
-//# sourceMappingURL=Text.stories-6682df89.js.map
